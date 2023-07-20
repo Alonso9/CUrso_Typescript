4dd1d86e807180568454a8f22052eba9edb8cf24
Alonso9 <60718869+Alonso9@users.noreply.github.com>
2023-07-19 18:25:10 -0600

    Se termino el video de estructuracion de funciones

---
+++ topics/06-function-destructuring.ts
@@ -0,0 +1,51 @@
+interface Products {
+  description: string;
+  price: number;
+}
+
+const phone: Products = {
+  description: 'Nokia 360',
+  price: 120.5,
+};
+
+const tablet: Products = {
+  description: 'IPad Air',
+  price: 215.5,
+};
+
+interface TaxCalculationOptions {
+  tax: number;
+  products: Products[];
+}
+
+function taxCalculation(options: TaxCalculationOptions): number[] {
+  let total = 0;
+
+  options.products.forEach((product) => {
+    total += product.price;
+  });
+
+  return [total, total * options.tax];
+}
+
+const shoppingCart = [phone, tablet];
+const tax = 0.15;
+
+const resultTax = taxCalculation({
+  products: shoppingCart,
+  tax: tax,
+});
+
+console.log('Total: ', resultTax[0]);
+console.log('Tax: ', resultTax[1]);
+
+/* Tarea de aplicar destructuracion */
+
+const [totalPrice, TotalTax] = taxCalculation({
+  products: shoppingCart,
+  tax: tax,
+});
+
+console.log('Total Des:', totalPrice);
+console.log('Tax Des:', TotalTax);
+export {};
